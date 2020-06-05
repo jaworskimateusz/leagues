0f464ef1ddbd72ad9af07d4a07ba1704a6bb2723
jaworski <jaworskimateuszm@gmail.com>
2020-06-05 18:49:32 +0200

    adding  sql script files

---
+++ scripts/create-user.sql
@@ -0,0 +1,15 @@
+CREATE USER 'student'@'localhost' IDENTIFIED BY 'student';
+
+GRANT ALL PRIVILEGES ON * . * TO 'student'@'localhost';
+
+#
+# Starting with MySQL 8.0.4, the MySQL team changed the 
+# default authentication plugin for MySQL server 
+# from mysql_native_password to caching_sha2_password.
+#
+# The command below will make the appropriate updates for your user account.
+#
+# See the MySQL Reference Manual for details: 
+# https://dev.mysql.com/doc/refman/8.0/en/caching-sha2-pluggable-authentication.html
+#
+ALTER USER 'student'@'localhost' IDENTIFIED WITH mysql_native_password BY 'student';
